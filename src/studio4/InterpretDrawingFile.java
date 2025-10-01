a2dc6d8d56b2e4fd7b9747b5459a8f4fb5a9092a
max-delgado <maxd@wustl.edu>
2025-10-01 15:02:13 -0500

    hi

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,15 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		//int color1 = in.nextInt();
+		//int color2 = in.nextInt();
+		//int color3 = in.nextInt();
+		double dim1 = in.nextDouble();
+		double dim2 = in.nextDouble();
+		double dim3 = in.nextDouble();
+		double dim4 = in.nextDouble();
+		//StdDraw.setPenColor(StdDraw.BLUE);
+		//StdDraw.filledEllipse(dim1, dim2, dim3, dim4);
+		System.out.println(dim1);
 	}
 }
