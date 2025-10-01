cf32931e79048798ebeaf7d86f3d283434ac8eb6
max-delgado <maxd@wustl.edu>
2025-10-01 15:37:15 -0500

    draw

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,10 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		double[] x = {0.05, 0.5, 0.95};
+		double[] y = {0.15, 0.95, 0.15};
+		StdDraw.polygon(x, y);
+		StdDraw.circle(0.5, 0.45, 0.45);
+		StdDraw.ellipse(0.5, 0.5, 0.2, 0.1);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,15 +19,16 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		//int color1 = in.nextInt();
-		//int color2 = in.nextInt();
-		//int color3 = in.nextInt();
+		String name = in.next();
+		int color1 = in.nextInt();
+		int color2 = in.nextInt();
+		int color3 = in.nextInt();
+		boolean filled = in.nextBoolean();
 		double dim1 = in.nextDouble();
 		double dim2 = in.nextDouble();
 		double dim3 = in.nextDouble();
 		double dim4 = in.nextDouble();
-		//StdDraw.setPenColor(StdDraw.BLUE);
-		//StdDraw.filledEllipse(dim1, dim2, dim3, dim4);
-		System.out.println(dim1);
+		StdDraw.setPenColor(color1, color2, color3);
+		StdDraw.filledEllipse(dim1, dim2, dim3, dim4);
 	}
 }
